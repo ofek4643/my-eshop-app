28d6186400947bb999bfe9e6f5ef6d4aa26894d9
ofek4643 <ofekddia1@gmail.com>
2025-12-04 03:06:26 +0200

    update api url with /api endpoint

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://eshop-project-react.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://eshop-project-react.onrender.com/api",
   withCredentials: true,
 });
